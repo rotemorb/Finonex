54aec98f0c403a17ad02ddc8757f95496de18562
Rotem Orbach <orbachrotem@gmail.com>
2022-08-04 14:28:08 +0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './main/main.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { BottomMenuComponent } from './bottom-menu/bottom-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LeftMenuComponent,
+    TopMenuComponent,
+    BottomMenuComponent
   ],
   imports: [
     BrowserModule,
