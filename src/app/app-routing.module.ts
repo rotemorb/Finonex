667daa35661111271899146e65d412675007ad19
Rotem Orbach <orbachrotem@gmail.com>
2022-08-04 20:42:10 +0300

    Fix routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PostsComponent } from './posts/posts.component';
-import { HomeComponent } from './home/home.component';
-/* import { MainComponent } from './main/main.component'; */
+
 const routes: Routes = [
   {
     path: '',
@@ -14,28 +14,26 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'posts',
+    component: PostsComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent,
+  },
+  {
     path: 'home',
     component: HomeComponent,
-    children: [
-      {
-        path: 'posts',
-        component: PostsComponent,
-      },
-      {
-        path: 'gallery',
-        component: GalleryComponent,
-      },
-      {
-        path: 'contact-us',
-        component: ContactUsComponent,
-      },
-    ],
   },
   { path: '**', component: NotFoundComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [],
   imports: [CommonModule, RouterModule.forRoot(routes)],
 })
 export class AppRoutingModule {}
