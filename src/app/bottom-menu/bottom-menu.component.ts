411f70f2aa530c466e5eb9901a78efd00c98c4f5
rotemorb <orbachrotem@gmail.com>
2022-08-05 10:55:45 +0300

    Menus are finished!

--- src/app/bottom-menu/bottom-menu.component.ts
+++ src/app/bottom-menu/bottom-menu.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bottom-menu',
   templateUrl: './bottom-menu.component.html',
-  styleUrls: ['./bottom-menu.component.css']
+  styleUrls: ['./bottom-menu.component.css'],
 })
 export class BottomMenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
